06e45f6d1ef4544b55ed6d5364da47da77b91f3a
YellowBenefits <contato@yellowbc.com.br>
2025-05-17 18:29:16 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
-// Placeholder for service-worker.js
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('yellow-cache').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'index.css',
+        'nicepage.css',
+        'jquery.js',
+        'nicepage.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
